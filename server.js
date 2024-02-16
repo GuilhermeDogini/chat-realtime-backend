866ae595d65e08fb68cd525a54df78c6bf18866c
Guilherme Dogini <52366238+GuilhermeDogini@users.noreply.github.com>
2024-02-16 00:01:16 -0300

    final server.js

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ io.on("connection", (socket) => {
 });
 
 app.get("/", (req, res) => {
-  return res.send("This is sheychat-next-udemy nodejs server");
+  return res.send("This is sheychat-next nodejs server");
 });
